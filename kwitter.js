675ec2a0e45d7661f6d1e90a4c07b2949f60782e
RishiPoosarla <88695245+RishiPoosarla@users.noreply.github.com>
2022-03-01 18:41:16 -0800

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,25 +1,4 @@
 
-//ADD YOUR FIREBASE LINKS HERE
-var firebaseConfig = {
-    apiKey: "AIzaSyBL3k0HblqiA2a_6Z0gTMmXwEC_pki80JU",
-    authDomain: "let-s-chat-app-1c2c2.firebaseapp.com",
-    databaseURL: "https://let-s-chat-app-1c2c2-default-rtdb.firebaseio.com",
-    projectId: "let-s-chat-app-1c2c2",
-    storageBucket: "let-s-chat-app-1c2c2.appspot.com",
-    messagingSenderId: "900508202046",
-    appId: "1:900508202046:web:1ff4719d09de56423acd6f",
-    measurementId: "G-CVXKQ50LWW"
-  };
-  
-  // Initialize Firebase
-   firebase.initializeApp(firebaseConfig);
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-     Room_names = childKey;
-    //Start code
-
-    //End code
-    });});}
-getData();
 function addUser(){
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
